2bc7d0323c3a3052d216167f3a391b0da28c81e2
frankod123 <frank_od123@yahoo.com>
2023-07-31 06:16:23 +0100

    add comments to json

--- routes/skate-spots.js
+++ routes/skate-spots.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-// const crypto = require("crypto");
+const crypto = require("crypto");
 
 router.get("/", (req, res) => {
   const skateSpots = JSON.parse(fs.readFileSync("./data/skate-spots.json"));
@@ -16,6 +16,24 @@ router.get("/:id", (req, res) => {
   });
 
   res.status(201).json(selectedSkateSpot);
+});
+
+router.post("/:id", (req, res) => {
+  const newComment = {
+    id: crypto.randomBytes(16).toString("hex"),
+    name: req.body.name,
+    comment: req.body.comment,
+    likes: "0",
+    timestamp: Date.now(),
+  };
+
+  const comments = JSON.parse(fs.readFileSync("./data/skate-spots.json"));
+
+  videos.push(newComment);
+
+  fs.writeFileSync("./data/skate-spots.json", JSON.stringify(comments));
+
+  res.status(201).json(newComment);
 });
 
 module.exports = router;
